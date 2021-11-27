d2ef54c7bb3d2798d6a7d8830914c6b6e59afff7
CJ Karkut <cjkarkut@gmail.com>
2021-11-26 23:48:03 -0600

    sync js

---
+++ scripts.js
@@ -0,0 +1,6 @@
+document.addEventListener("input", (event) => {
+    console.log(event);
+    if (event.target.id === "color" && event.target.value) {
+        document.body.style.setProperty('--stick-color', event.target.value);
+    }
+  });
